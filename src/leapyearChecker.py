4c88b4b3ebaad0aeed1c3f5c9f0013736b1d4921
shwetaTh <shwetaTh@users.noreply.github.com>
2024-10-03 20:29:50 +0545

    leapyear checker logic

--- src/leapyearChecker.py
+++ src/leapyearChecker.py
@@ -1,3 +1,9 @@
+# In the Gregorian calendar, three conditions are used to identify leap years:
+
+# The year can be evenly divided by 4, is a leap year, unless:
+# The year can be evenly divided by 100, it is NOT a leap year, unless:
+# The year is also evenly divisible by 400. Then it is a leap year.
+
 def is_leap(year):
     leap = False
     
