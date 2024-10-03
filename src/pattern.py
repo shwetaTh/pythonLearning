6a9ca3ea44b2708a5e482030673601fbde5cb9cf
shwetaTh <shwetaTh@users.noreply.github.com>
2024-10-03 20:53:01 +0545

    creating patterns with loop

---
+++ src/pattern.py
@@ -0,0 +1,14 @@
+n = int(input("Enter range: "))
+# i=1
+# while i<=n:
+#     for _ in range(i):
+#         print("*", end='')
+#     print()
+#     i+=1
+
+i=n
+while i>=1:
+    for _ in range(i):
+        print("*", end='')
+    print()
+    i-=1    
