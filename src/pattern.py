9cc0e381a4083ef32447e01bc9515594809a1be3
shwetaTh <shwetaTh@users.noreply.github.com>
2024-10-04 18:00:23 +0545

    pyramid pattern

--- src/pattern.py
+++ src/pattern.py
@@ -6,9 +6,25 @@ n = int(input("Enter range: "))
 #     print()
 #     i+=1
 
-i=n
-while i>=1:
-    for _ in range(i):
+# i=n
+# while i>=1:
+#     for _ in range(i):
+#         print("*", end='')
+#     print()
+#     i-=1  
+
+
+i = 1  # Start with the first row
+while i <= n:
+    # Print spaces (n - i) spaces
+    for _ in range(n - i):
+        print(" ", end='')
+    
+    # Print asterisks (2 * i - 1) asterisks
+    for _ in range(2 * i - 1):
         print("*", end='')
+    
+    # Move to the next line after printing spaces and asterisks
     print()
-    i-=1    
+    
+    i += 1  # Move to the next row                 
